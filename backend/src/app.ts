ba1614d07c06e4fd810fa28dd7744b3faa85f7ea
Irving Zamora <irvingzamora28@gmail.com>
2022-02-22 17:31:25 -0700

    Add error handler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { json } from "body-parser";
 import { productRouter } from "./routes/expenseRoutes";
+import { errorHandler } from "./middleware/errorMiddleware";
 
 const app = express();
 app.use(json());
 app.use(express.urlencoded({ extended: false }));
 app.use(productRouter);
+app.use(errorHandler)
 
 export default app;
--- backend/src/controllers/expenseController.ts
+++ backend/src/controllers/expenseController.ts
@@ -1,39 +1,39 @@
-import { NextFunction, Request, Response } from "express"
-
+import { NextFunction, Request, Response } from "express";
 
 const ExpenseController = () => {
-    
-    const index = (req:Request, res: Response, next: NextFunction) => {
-        res.status(200).send({message: "Get expenses"})
-    }
+	const index = (req: Request, res: Response, next: NextFunction) => {
+		res.status(200).send({ message: "Get expenses" });
+	};
 
-    const create = (req:Request, res: Response, next: NextFunction) => {
-        console.log(req.body);
-        
-        res.status(200).send({message: "Create expense"})
-    }
+	const create = (req: Request, res: Response, next: NextFunction) => {
+		console.log(req.body);
+		if (!req.body.name) {
+			res.status(400);
+			throw new Error("Please add the name of the expense");
+		}
 
-    const get = (req:Request, res: Response, next: NextFunction) => {
-        res.status(200).send({message: "Get expense"})
+		// res.status(200).send({ message: "Create expense" });
+	};
 
-    }
+	const get = (req: Request, res: Response, next: NextFunction) => {
+		res.status(200).send({ message: "Get expense" });
+	};
 
-    const update = (req:Request, res: Response, next: NextFunction) => {
-        res.status(200).send({message: "Update expense"})
+	const update = (req: Request, res: Response, next: NextFunction) => {
+		res.status(200).send({ message: "Update expense" });
+	};
 
-    }
+	const destroy = (req: Request, res: Response, next: NextFunction) => {
+		res.status(200).send({ message: "Delete expense" });
+	};
 
-    const destroy = (req:Request, res: Response, next: NextFunction) => {
-        res.status(200).send({message: "Delete expense"})
-    }
+	return {
+		index,
+		create,
+		get,
+		update,
+		destroy,
+	};
+};
 
-    return {
-        index,
-        create,
-        get,
-        update,
-        destroy
-    }
-}
-
-export default ExpenseController
+export default ExpenseController;
