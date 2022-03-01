b74b9abc2a96c137b957bebcc8fccf7830111242
Irving Zamora <irvingzamora28@gmail.com>
2022-02-28 22:32:24 -0700

    Restrict Home page to authenticated users only

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { RootState } from '../app/store'
 
 const Home = () => {
+    const navigate = useNavigate()
+
+    const {user} = useSelector((state: RootState) => state.auth)
+
+    useEffect(() => {
+      if (!user) {
+          navigate("/login")
+      }
+    
+    }, [user, navigate])
+    
     return (
         <div>
             Hello world
