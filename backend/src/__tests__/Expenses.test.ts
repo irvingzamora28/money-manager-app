51f706aff0294d4b9efbd22be61a3527e9beee94
Irving Zamora <irvingzamora28@gmail.com>
2022-03-07 19:41:41 -0700

    Add jest and supertest

---
+++ backend/src/__tests__/Expenses.test.ts
@@ -0,0 +1,10 @@
+describe("ExpenseService", () => {
+	describe("given a call to method all", () => {
+
+		test("should return all expenses", async () => {
+			expect(true == true);
+		});
+		
+	});
+	
+});
