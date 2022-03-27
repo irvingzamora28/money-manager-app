f12b66af1c3bc0bbc571b41beb45285a260afe29
Irving Zamora <irvingzamora28@gmail.com>
2022-03-27 14:17:24 -0600

    Update character limit of categories icon to 1024

--- backend/src/helpers/categoryValidation.ts
+++ backend/src/helpers/categoryValidation.ts
@@ -4,7 +4,7 @@ const categoryCreateSchema = Joi.object({
 	name: Joi.string().min(3).max(30).required().pattern(/^[\w\-\,\.\(\)\s]*[^\W_\-\,\.\(\)\s][\w\-\,\.\(\)\s]*$/).messages({
         'string.pattern.base': `"name" is not valid, it can only contain letters, numbers and (,._)`
       }),
-	icon: Joi.string().min(3).max(128).required(),
+	icon: Joi.string().min(3).max(1024).required(),
 });
 
 const categoryUpdateSchema = Joi.object({
@@ -12,7 +12,7 @@ const categoryUpdateSchema = Joi.object({
     .messages({
         'string.pattern.base': `"name" is not valid, it can only contain letters, numbers and (,._)`
       }),
-	icon: Joi.string().min(3).max(128),
+	icon: Joi.string().min(3).max(1024),
 });
 
 export { categoryCreateSchema, categoryUpdateSchema };
