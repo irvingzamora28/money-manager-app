655ef509b9e7fbd7f1a82fc5a9d656ec05729a92
Irving Zamora <irvingzamora28@gmail.com>
2022-03-28 23:20:24 -0600

    Change design of table with list of categories

--- frontend/src/pages/Categories.tsx
+++ frontend/src/pages/Categories.tsx
@@ -8,10 +8,10 @@ const Categories = () => {
         if (element && element.parentElement && element.parentElement.parentElement) {
             console.log(element);
             console.log("element");
-            
+
             var dropdowns = document.getElementsByClassName('dropdown-content');
             console.log(dropdowns);
-            
+
             let list = element.parentElement.parentElement.getElementsByClassName('dropdown-content')[0];
             list.classList.add('target');
             for (let idx = 0; idx < dropdowns.length; idx++) {
@@ -47,7 +47,7 @@ const Categories = () => {
 
     const dropdownFunctionSVG = (event: React.MouseEvent<SVGSVGElement>) => {
         console.log("Clicked SVG");
-        
+
         // let element = event.target;
         // if (element != null) {
         //     var dropdowns = document.getElementsByClassName('dropdown-content');
@@ -66,7 +66,7 @@ const Categories = () => {
 
     return (
         <div>
-            <CategoryFormModal />
+            {/* <CategoryFormModal /> */}
             <div className="sm:px-6 w-full">
                 <div className="px-4 md:px-10 py-0 md:pt-4">
                     <div className="flex items-center justify-between">
@@ -96,63 +96,24 @@ const Categories = () => {
                                     </td>
                                     <td className="">
                                         <div className="flex items-center pl-5">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-black mr-2" viewBox="0 0 576 512"><path d="M568.1 196.3l-22.62-22.62c-4.533-4.533-10.56-7.029-16.97-7.029s-12.44 2.496-16.97 7.029l-5.654 5.656l-20.12-20.12c4.596-23.46-2.652-47.9-19.47-64.73l-45.25-45.25C390.2 17.47 347.1 0 303.1 0C258.2 0 216 17.47 184.3 49.21L176.5 57.05L272.5 105.1v13.81c0 18.95 7.688 37.5 21.09 50.91l49.16 49.14c13.44 13.45 31.39 20.86 50.54 20.86c4.758 0 9.512-.4648 14.18-1.387l20.12 20.12l-5.654 5.654c-9.357 9.357-9.357 24.58-.002 33.94l22.62 22.62c4.535 4.533 10.56 7.031 16.97 7.031s12.44-2.498 16.97-7.031l90.53-90.5C578.3 220.8 578.3 205.6 568.1 196.3zM270.9 192.4c-3.846-3.846-7.197-8.113-10.37-12.49l-239.5 209.2c-28.12 28.12-28.16 73.72-.0371 101.8C35.12 505 53.56 512 71.1 512s36.84-7.031 50.91-21.09l209.1-239.4c-4.141-3.061-8.184-6.289-11.89-9.996L270.9 192.4z"/></svg>
                                             <p className="text-base font-medium leading-none text-gray-700 mr-2">Marketing Keynote Presentation</p>
                                         </div>
                                     </td>
-                                    <td className="pl-5">
-                                        <div className="flex items-center">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                                <path d="M7.5 5H16.6667" stroke="#52525B" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                <path d="M7.5 10H16.6667" stroke="#52525B" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                <path d="M7.5 15H16.6667" stroke="#52525B" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                <path d="M4.16669 5V5.00667" stroke="#52525B" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                <path d="M4.16669 10V10.0067" stroke="#52525B" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                <path d="M4.16669 15V15.0067" stroke="#52525B" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
-                                            </svg>
-                                            <p className="text-sm leading-none text-gray-600 ml-2">04/07</p>
-                                        </div>
-                                    </td>
-                                    <td className="pl-5">
-                                        <div className="flex items-center">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                                <path d="M3.33331 17.4998V6.6665C3.33331 6.00346 3.59671 5.36758 4.06555 4.89874C4.53439 4.4299 5.17027 4.1665 5.83331 4.1665H14.1666C14.8297 4.1665 15.4656 4.4299 15.9344 4.89874C16.4033 5.36758 16.6666 6.00346 16.6666 6.6665V11.6665C16.6666 12.3295 16.4033 12.9654 15.9344 13.4343C15.4656 13.9031 14.8297 14.1665 14.1666 14.1665H6.66665L3.33331 17.4998Z" stroke="#52525B" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                <path d="M10 9.1665V9.17484" stroke="#52525B" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                <path d="M6.66669 9.1665V9.17484" stroke="#52525B" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                <path d="M13.3333 9.1665V9.17484" stroke="#52525B" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
-                                            </svg>
-                                            <p className="text-sm leading-none text-gray-600 ml-2">23</p>
-                                        </div>
-                                    </td>
-                                    <td className="pl-5">
-                                        <div className="flex items-center">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                                <path
-                                                    d="M12.5 5.83339L7.08333 11.2501C6.75181 11.5816 6.56556 12.0312 6.56556 12.5001C6.56556 12.9689 6.75181 13.4185 7.08333 13.7501C7.41485 14.0816 7.86449 14.2678 8.33333 14.2678C8.80217 14.2678 9.25181 14.0816 9.58333 13.7501L15 8.33339C15.663 7.67034 16.0355 6.77107 16.0355 5.83339C16.0355 4.8957 15.663 3.99643 15 3.33339C14.337 2.67034 13.4377 2.29785 12.5 2.29785C11.5623 2.29785 10.663 2.67034 10 3.33339L4.58333 8.75005C3.58877 9.74461 3.03003 11.0935 3.03003 12.5001C3.03003 13.9066 3.58877 15.2555 4.58333 16.2501C5.57789 17.2446 6.92681 17.8034 8.33333 17.8034C9.73985 17.8034 11.0888 17.2446 12.0833 16.2501L17.5 10.8334"
-                                                    stroke="#52525B"
-                                                    strokeWidth="1.25"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                ></path>
-                                            </svg>
-                                            <p className="text-sm leading-none text-gray-600 ml-2">04/07</p>
-                                        </div>
-                                    </td>
-                                    <td className="pl-5">
-                                        <button className="py-3 px-3 text-sm focus:outline-none leading-none text-red-700 bg-red-100 rounded">Due today at 18:00</button>
-                                    </td>
+                                   
                                     <td className="pl-4">
                                         <button className="focus:ring-2 focus:ring-offset-2 focus:ring-red-300 text-sm leading-none text-gray-600 py-3 px-5 bg-gray-100 rounded hover:bg-gray-200 focus:outline-none">View</button>
                                     </td>
                                     <td>
                                         <div className="relative px-5 pt-2">
-                                            <button className="focus:ring-2 rounded-md focus:outline-none" onClick={dropdownFunction} onBlur={dropdownFunctionOpposite} role="button" aria-label="option">
+                                            <button className="focus:ring-2 rounded-md focus:outline-none" onClick={dropdownFunction} onBlur={dropdownFunctionOpposite} aria-label="option">
                                                 <svg className="dropbtn" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                                                     <path d="M4.16667 10.8332C4.62691 10.8332 5 10.4601 5 9.99984C5 9.5396 4.62691 9.1665 4.16667 9.1665C3.70643 9.1665 3.33334 9.5396 3.33334 9.99984C3.33334 10.4601 3.70643 10.8332 4.16667 10.8332Z" stroke="#9CA3AF" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
                                                     <path d="M10 10.8332C10.4602 10.8332 10.8333 10.4601 10.8333 9.99984C10.8333 9.5396 10.4602 9.1665 10 9.1665C9.53976 9.1665 9.16666 9.5396 9.16666 9.99984C9.16666 10.4601 9.53976 10.8332 10 10.8332Z" stroke="#9CA3AF" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
                                                     <path d="M15.8333 10.8332C16.2936 10.8332 16.6667 10.4601 16.6667 9.99984C16.6667 9.5396 16.2936 9.1665 15.8333 9.1665C15.3731 9.1665 15 9.5396 15 9.99984C15 10.4601 15.3731 10.8332 15.8333 10.8332Z" stroke="#9CA3AF" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
                                                 </svg>
                                             </button>
-                                            <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 hidden">
+                                            <div className="dropdown-content bg-white shadow w-24 absolute z-30 mr-6 hidden">
                                                 <div tabIndex={0} className="focus:outline-none focus:text-indigo-600 text-xs w-full hover:bg-indigo-500 py-4 px-4 cursor-pointer hover:text-white">
                                                     <p>Edit</p>
                                                 </div>
